aeba161490006a6cf0f4efe01d64709bc3bdb88b
Gema Akbar <gema@sevva.co>
2018-06-13 03:16:44 +0700

    config app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,3 +1,6 @@
+import VueRouter from 'vue-router';
+import {routes} from './routes';
+import store from './store';
 
 /**
  * First we will load all of this project's JavaScript dependencies which
@@ -9,6 +12,15 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+
+Vue.use(VueRouter);
+
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+});
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -17,6 +29,15 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
+Vue.component('home', require('./components/Home.vue'));
+Vue.component('posting', require('./components/Posting.vue'));
+Vue.component('navbar', require('./components/Navbar.vue'));
+Vue.component('status', require('./components/Status.vue'));
+Vue.component('status-feed', require('./components/StatusFeed.vue'));
+Vue.component('comment', require('./components/Comment.vue'));
+
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router,
+    store
 });
